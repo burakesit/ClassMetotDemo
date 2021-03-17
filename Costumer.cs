99a2b0da3390460e885973502ff54569fb738f7a
burak eşit <esitburak@hotmail.com>
2021-03-17 22:02:44 +0300

    Add project files.

---
+++ Costumer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class Costumer
+    {
+        public string CostumerName { get; set; }
+        public string CostumerSurname { get; set; }
+        public string Id { get; set; }
+        public int Id { get; set; }
+        public int Age { get; set; }
+        public int Gender { get; set; }
+
+    }
+}
